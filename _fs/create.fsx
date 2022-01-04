5c746b971213f7e6af306cc8c08b706300f16b28
Ching-Hua Chen <info@tailoryourbim.com>
2022-01-04 23:05:09 +0100

    Update
    thanks.html got a back link

--- _fs/create.fsx
+++ _fs/create.fsx
@@ -2,7 +2,6 @@
 //#load "/Users/ching/Documents/GoogleDrive/My Drive/__lib/fs/web_create.fsx"
 //#load @"C:\Dropbox\__lib\fs/web_create.fsx"
 
-let title = "Use Add-In Manager from Autodesk"
+let title = "Compile F# Codes Without Closing Revit"
 let path = __SOURCE_DIRECTORY__
 title |> Web_create.newPost path
-
