b65b92aed484ee1d6fdb8f891340ee1244a9e4f7
Ching-Hua Chen <info@tailoryourbim.com>
2023-03-12 22:41:55 +0100

    update

--- _fs/create.fsx
+++ _fs/create.fsx
@@ -1,7 +1,7 @@
-#load "/Users/ching/Dropbox/__lib/fs/web_create.fsx"
+#load "../../../__lib/fs/web_create.fsx"
 //#load "/Users/ching/Documents/GoogleDrive/My Drive/__lib/fs/web_create.fsx"
 //#load @"C:\Dropbox\__lib\fs/web_create.fsx"
 
-let title = "Set up an F# Solution for tyRCore"
+let title = "Quick Note: What you need to manipulte Ifc file."
 let path = __SOURCE_DIRECTORY__
 title |> Web_create.newPost path
