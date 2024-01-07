df4eae1ec0b642f7de317e32d679a7e26c127773
Ching <info@chings.eu>
2024-01-07 23:19:05 +0100

    upload tyifc

--- _fs/create.fsx
+++ _fs/create.fsx
@@ -2,6 +2,6 @@
 //#load "/Users/ching/Documents/GoogleDrive/My Drive/__lib/fs/web_create.fsx"
 //#load @"C:\Dropbox\__lib\fs/web_create.fsx"
 
-let title = "Quick Note: What you need to manipulte Ifc file."
+let title = "Tailor Your Ifc with openbim-components."
 let path = __SOURCE_DIRECTORY__
 title |> Web_create.newPost path
