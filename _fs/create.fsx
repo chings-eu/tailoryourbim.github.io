150ea8fb46d45052c7254b2d28c69afee77adf9b
Ching <info@chings.eu>
2024-07-09 15:10:45 +0200

    update

--- _fs/create.fsx
+++ _fs/create.fsx
@@ -3,6 +3,6 @@
 //#load "/Users/ching/Documents/GoogleDrive/My Drive/__lib/fs/web_create.fsx"
 //#load @"C:\Dropbox\__lib\fs/web_create.fsx"
 
-let title = "How to Investigate Your Ifc Model with IfcOpenShell and Jupyter Notebook"
+let title = "Ask our GPT About F# Coding for Revit"
 let path = __SOURCE_DIRECTORY__
 title |> Web_create.newPost path
---
+++ _posts/2024/2024-06-24-ask-our-gpt-about-f#-coding-for-revit.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "Ask our GPT About F# Coding for Revit"
+date: 2024-06-24-154258 
+categories: 
+tags: 
+published: false
+---
+<script src="/assets/signup/signup.js"></script>
+<signup-component></signup-component>
+
+## A Header
